a9293beb02755429e11db62940767bd29bbc7182
ramduvvuri <ram@axone-tech.uk>
2020-02-02 08:33:32 +0000

    Added sys out statement and comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I love Git");
+        System.out.println("I love SCM");
+        //Comments
+        
     }
 }
